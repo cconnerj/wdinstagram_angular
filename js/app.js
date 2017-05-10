ef30b9d0e2ba6ca4c0b6ae395cbdb3c729cffa02
Conner Johnson <Conner.johnson33@gmail.com>
2017-05-10 08:33:34 -0400

    show index

--- js/app.js
+++ js/app.js
@@ -1,5 +1,40 @@
 "use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+
+angular
+ .module("instagram", [
+   "ui.router",
+   "ngResource"
+ ])
+ .config([
+   "$stateProvider",
+   Router])
+ .controller("InstagramIndexController", [
+   "InstagramFactory",
+   InstagramIndexControllerFunction
+ ])
+ .controller("InstagramShowController",[
+   "InstagramFactory",
+   InstagramShowControllerFunction
+ ])
+ .factory("InstagramFactory",[
+   "$resource",
+   FactoryFunction
+ ])
+
+ function Router($stateProvider){
+   $stateProvider
+   .state("InstagramIndex", {
+     url: "/instagrams",
+     controller: "InstagramIndexController",
+   	controllerAs: "vm"
+  });
+  }
+
+   function InstagramIndexControllerFunction(InstagramFactory){
+   	this.instagrams= InstagramFactory.query()
+   }
+
+   function InstagramShowControllerFunction(){
+    this.instagram= InstagramFactory.get({id: $stateParams.id})
+   }
